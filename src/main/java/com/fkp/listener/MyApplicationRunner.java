74be1f83f70c610810698ed776ef14920ee99229
fkp <1320793853@qq.com>
2022-10-11 22:12:32 +0800

    1.添加ApplicationRunner 2.添加.iml,没有此文件在idea中运行项目不会自动编译

---
+++ src/main/java/com/fkp/listener/MyApplicationRunner.java
@@ -0,0 +1,19 @@
+package com.fkp.listener;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(value = 1)
+public class MyApplicationRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("MyApplicationRunner....");
+        System.out.println(args);
+        new Thread(() -> {
+            System.out.println("ssadas");
+        }).start();
+    }
+}
