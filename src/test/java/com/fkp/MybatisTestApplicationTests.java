2e0cf6f7d6c7d640a07927bed5d00b8921b15b6a
fkp <1320793853@qq.com>
2022-10-18 23:52:23 +0800

    添加.idea和.iml文件，否则会出现修改代码后idea不自动编译。

--- src/test/java/com/fkp/MybatisTestApplicationTests.java
+++ src/test/java/com/fkp/MybatisTestApplicationTests.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.net.Inet4Address;
 import java.net.InetSocketAddress;
@@ -34,54 +35,8 @@ class MybatisTestApplicationTests {
 	@Autowired
 	private TestMapper testMapper;
 
-	@Test
-	void contextLoads() {
-		PageHelper.startPage(2,2);
-		List<KmsServer> kmsServers = testMapper.selectList(null);
-		PageInfo<KmsServer> pageInfo = new PageInfo<>(kmsServers);
-		List<KmsServer> list = pageInfo.getList();
-		int pageSize = pageInfo.getPageSize();
-		int pageNum = pageInfo.getPageNum();
-		long total = pageInfo.getTotal();
-		System.out.println(pageNum);
-		System.out.println(pageSize);
-		System.out.println(total);
-		System.out.println(JSON.toJSONString(list,SerializerFeature.WriteNullStringAsEmpty));
-	}
-
-	@Test
-	public void test(){
-		KmsServer kmsServer = new KmsServer();
-		kmsServer.setId("001");
-		int insert = testMapper.insert(kmsServer);
-		System.out.println(insert);
-	}
-
-	@Test
-	public void test2(){
-		Page<KmsServer> page = new Page<>();
-		page.setCurrent(1);
-		page.setSize(2);
-		Page<KmsServer> page1 = testMapper.selectPage(page, null);
-		System.out.println(page1.getCurrent());
-		System.out.println(page1.getSize());
-		System.out.println(page1.getTotal());
-		System.out.println(page1.getRecords().size());
-	}
-
-	@Test
-	public void test3(){
-		Page<KmsServer> page = new Page<>();
-		page.setCurrent(1);
-		page.setSize(2);
-		KmsServer obj = new KmsServer();
-		obj.setId("1");
-		Page<KmsServer> page1 = testMapper.findPage22(page, obj);
-		System.out.println(page1.getCurrent());
-		System.out.println(page1.getSize());
-		System.out.println(page1.getTotal());
-		System.out.println(page1.getRecords().size());
-	}
+	@Autowired
+	private ThreadPoolTaskExecutor executor;
 
 	@Test
 	public void test4(){
@@ -103,6 +58,57 @@ class MybatisTestApplicationTests {
 		mapperUtils.closeSqlSession();
 		System.out.println(s);
 		System.out.println(s2);
+		List<KmsServer> kmsServers = testMapper.selectList(null);
+		System.out.println(kmsServers);
 	}
+
+//	@Test
+//	void contextLoads() {
+//		PageHelper.startPage(2,2);
+//		List<KmsServer> kmsServers = testMapper.selectList(null);
+//		PageInfo<KmsServer> pageInfo = new PageInfo<>(kmsServers);
+//		List<KmsServer> list = pageInfo.getList();
+//		int pageSize = pageInfo.getPageSize();
+//		int pageNum = pageInfo.getPageNum();
+//		long total = pageInfo.getTotal();
+//		System.out.println(pageNum);
+//		System.out.println(pageSize);
+//		System.out.println(total);
+//		System.out.println(JSON.toJSONString(list,SerializerFeature.WriteNullStringAsEmpty));
+//	}
+//
+//	@Test
+//	public void test(){
+//		KmsServer kmsServer = new KmsServer();
+//		kmsServer.setId("001");
+//		int insert = testMapper.insert(kmsServer);
+//		System.out.println(insert);
+//	}
+//
+//	@Test
+//	public void test2(){
+//		Page<KmsServer> page = new Page<>();
+//		page.setCurrent(1);
+//		page.setSize(2);
+//		Page<KmsServer> page1 = testMapper.selectPage(page, null);
+//		System.out.println(page1.getCurrent());
+//		System.out.println(page1.getSize());
+//		System.out.println(page1.getTotal());
+//		System.out.println(page1.getRecords().size());
+//	}
+//
+//	@Test
+//	public void test3(){
+//		Page<KmsServer> page = new Page<>();
+//		page.setCurrent(1);
+//		page.setSize(2);
+//		KmsServer obj = new KmsServer();
+//		obj.setId("1");
+//		Page<KmsServer> page1 = testMapper.findPage22(page, obj);
+//		System.out.println(page1.getCurrent());
+//		System.out.println(page1.getSize());
+//		System.out.println(page1.getTotal());
+//		System.out.println(page1.getRecords().size());
+//	}
 
 }
