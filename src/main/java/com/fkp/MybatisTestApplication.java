86872b903ef6d6bface5026ffd4233e235522561
fengkunpeng <fkp123456>
2023-02-01 17:01:22 +0800

    1.清理无用代码

--- src/main/java/com/fkp/MybatisTestApplication.java
+++ src/main/java/com/fkp/MybatisTestApplication.java
@@ -1,15 +1,9 @@
 package com.fkp;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.fkp.util.SqlSessionFactoryUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.fkp.mapper")
@@ -19,19 +13,4 @@ public class MybatisTestApplication {
 		SpringApplication.run(MybatisTestApplication.class, args);
 		System.out.println(SqlSessionFactoryUtils.sqlSessionFactory);
 	}
-
-	private final String dbtype = "mysql";
-//	@Bean
-//	public MybatisPlusInterceptor paginationInterceptor() {
-//		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-//		InnerInterceptor innerInterceptor = null;
-//		if(StringUtils.isBlank(dbtype)){
-//			innerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
-//		}else{
-//			innerInterceptor = new PaginationInnerInterceptor(DbType.getDbType(dbtype));
-//		}
-//		interceptor.addInnerInterceptor(innerInterceptor);
-//		return interceptor;
-//	}
-
 }
