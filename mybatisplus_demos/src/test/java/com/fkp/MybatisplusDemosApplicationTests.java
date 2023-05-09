8e24c5bbb4fc935ed789d36f33f78ac1598646a6
fengkunpeng <fkp123456>
2023-05-09 18:46:25 +0800

    测试模块

--- mybatisplus_demos/src/test/java/com/fkp/MybatisplusDemosApplicationTests.java
+++ mybatisplus_demos/src/test/java/com/fkp/MybatisplusDemosApplicationTests.java
@@ -1,5 +1,6 @@
 package com.fkp;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.fkp.test.entity.KmsServer;
 import com.fkp.test.mapper.KmsServerMapper;
 import org.junit.jupiter.api.Test;
@@ -16,7 +17,7 @@ class MybatisplusDemosApplicationTests {
 
     @Test
     void contextLoads() {
-        List<KmsServer> kmsServers = kmsServerMapper.selectList(null);
+        List<KmsServer> kmsServers = kmsServerMapper.selectList(new LambdaQueryWrapper<KmsServer>(KmsServer.class).eq(KmsServer::getId, "1"));
         System.out.println(kmsServers);
     }
 
