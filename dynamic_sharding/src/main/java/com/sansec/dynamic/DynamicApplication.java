bf3c8065756bca5c1f1cfdf49249a036f54a3664
fengkunpeng <fkp123456>
2023-11-15 18:31:51 +0800

    1.dynamic-sharding，项目启动

--- dynamic_sharding/src/main/java/com/sansec/dynamic/DynamicApplication.java
+++ dynamic_sharding/src/main/java/com/sansec/dynamic/DynamicApplication.java
@@ -1,16 +1,29 @@
 package com.sansec.dynamic;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.sansec.dynamic.mapper.BusinessUserMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+import javax.sql.DataSource;
+
+/**
+ * @author fengkunpeng
+ */
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
 @MapperScan(basePackages = "com.sansec.dynamic.mapper")
+@EnableScheduling
 public class DynamicApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DynamicApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(DynamicApplication.class, args);
+		DataSource bean = run.getBean(DataSource.class);
+		System.out.println(bean);
 	}
 
 }
